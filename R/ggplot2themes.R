65ed5d3aacbd4cd7e0e43ed8d43077d33c54820e
rasi <rasi1983@gmail.com>
2018-04-15 11:28:49 -0700

    autogenerate NAMESPACE from Roxygen2

--- R/ggplot2themes.R
+++ R/ggplot2themes.R
@@ -4,13 +4,13 @@
 #' all font sizes set to size 8, axis line width at 0.25pt, and all axis text
 #' and lines in set to black.
 #'
-#' @param NA
 #' @return old theme
 #' @export
+#' @import ggplot2
 theme_rasilab <- function() {
   ggplot2::theme_classic(base_family = "Helvetica", base_size = 8) +
     ggplot2::theme(
-      strip.background = element_blank(),
+      strip.background = ggplot2::element_blank(),
       legend.text = element_text(size = 8),
       strip.text.x = element_text(size = 8),
       axis.line = element_line(color = "black", size=.25),
