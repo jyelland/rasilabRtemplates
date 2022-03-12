9700ee5154a1adfb100c3cd638edabb48381c94d
rasi <rasi1983@gmail.com>
2022-03-12 14:53:10 -0800

    Increase font size

--- R/ggplot2themes.R
+++ R/ggplot2themes.R
@@ -8,13 +8,13 @@
 #' @export
 #' @import ggplot2
 theme_rasilab <- function() {
-  theme_classic(base_family = "Helvetica", base_size = 10) +
+  theme_classic(base_family = "Helvetica", base_size = 12) +
     theme(
       strip.background = ggplot2::element_blank(),
-      legend.text = element_text(size = 10),
-      strip.text.x = element_text(size = 10),
+      legend.text = element_text(size = 12),
+      strip.text.x = element_text(size = 12),
       axis.line = element_line(color = "black", size=.25),
-      axis.text = element_text(color = "black", size = 10)
+      axis.text = element_text(color = "black", size = 12)
     )
 }
 
