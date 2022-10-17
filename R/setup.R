6a9ba88b8308d8363e5299b73a2f04ee30699c18
rasi <rasi1983@gmail.com>
2022-10-17 12:50:16 -0700

    Make fill color-blind friendly

--- R/setup.R
+++ R/setup.R
@@ -7,6 +7,7 @@
   ggplot2::theme_set(theme_rasilab())
   # set default color to color-blind friendly palette
   options(ggplot2.discrete.colour = cbPalette)
+  options(ggplot2.discrete.fill = cbPalette)
   # optimize plot width for Jupyter notebook
   options(repr.plot.width=4, repr.plot.height=3)
   # decrease number of rows shown
